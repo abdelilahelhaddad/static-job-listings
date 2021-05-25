81b8ab6a9f4975d70c9d6ebdd3c8dd28b3cb2a9e
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-05-25 19:00:20 +0100

    :iphone: :seedling: Fetch data from JSON & display it :sparkles: :iphone:

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
 const main = document.querySelector("#main");
 
-function getJobsData() {
-  axios
-    .get("data.json")
-    .then(res => jobBox(res))
-    .catch(err => console.error(err));
+const fetchJobs = async () => {
+  await getJobs();
 }
-// fetch("./data.json")
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => console.log(data));
 
-function jobBox(res) {
-  let jobsData = res.data;
-  for (let i = 0; i < jobsData.length; i++) {
-    main.innerHTML = `
-    <div class="box container">
-    <img class="logo" src="${jobsData[i].logo}" alt="">
-    <div class="content">
-      <div class="company mb">
-        <span class="companyName">${jobsData[i].company}</span>
-        <p class="${jobsData[i].new} new">New!</p>
-        <p class="${jobsData[i].featured} featured">Featured</p>
-      </div>
-      <div class="position mb">
-        <p>${jobsData[i].position}</p>
-      </div>
-      <div class="postInfos mb">
-        <p>${jobsData[i].postedAt}</p>
-        <p>${jobsData[i].contract}</p>
-        <p>${jobsData[i].location}</p>
-      </div>
+const getJobs = async () => {
+  const url = "data.json";
+  const res = await fetch(url);
+  const data = await res.json();
+  createJobs(data);
+}
+
+const createJobs = (job) => {
+  for (let i = 0; i < job.length; i++) {
+    const jobEl = document.createElement('div');
+    jobEl.classList.add('box');
+    jobEl.classList.add('container');
+
+    const jobInnerHTML = `
+  <img class="logo" src="${job[i].logo}" alt="">
+  <div class="content">
+    <div class="company mb">
+      <span class="companyName">${job[i].company}</span>
+      <p class="new">New!</p>
+      <p class="featured">Featured</p>
     </div>
-    <div class="tags">
-        <span class="tagBg">Frontend</span>
-        <span class="tagBg">Senior</span>
-        <span class="tagBg">HTML</span>
-        <span class="tagBg">CSS</span>
-        <span class="tagBg">JavaScript</span>
-      </div>
-    `
+    <div class="position mb">
+      <p>${job[i].position}</p>
+    </div>
+    <div class="postInfos mb">
+      <p>${job[i].postedAt}</p>
+      <p>${job[i].contract}</p>
+      <p>${job[i].location}</p>
+    </div>
+  </div>
+  <div class="tags">
+    <span class="tagBg">Frontend</span>
+    <span class="tagBg">Senior</span>
+    <span class="tagBg">HTML</span>
+    <span class="tagBg">CSS</span>
+    <span class="tagBg">JavaScript</span>
+  </div>
+  `;
+
+    jobEl.innerHTML = jobInnerHTML;
+    main.appendChild(jobEl);
   }
 }
 
-document.addEventListener("DOMContentLoaded", getJobsData());
+fetchJobs();
